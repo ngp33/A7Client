23c40b4e9f6827083dbd0c468f722d102946c225
HaberR <rafaelhaber@dhcp-ccc-3576.eduroam.cornell.edu>
2015-10-15 19:09:42 -0400

    qerf

--- mutation/ParentConsciousMutation.java
+++ mutation/ParentConsciousMutation.java
@@ -7,7 +7,7 @@ import ast.Twokids;
 
 public abstract class ParentConsciousMutation extends MutationImpl {
 	
-	Node parent;
+	public Node parent;
 	
 	//Will add methods depending on how mutations are implemented in the main program
 	
--- testing/MutationTest.java
+++ testing/MutationTest.java
@@ -16,7 +16,7 @@ import parse.Tokenizer;
 
 public class MutationTest {
 
-	@Ignore
+	
 	@Test
 	public void remove() throws SyntaxError {
 		StringReader s = new StringReader("POSTURE != 17 --> POSTURE := 17; nearby[3] = 0 and ENERGY > 2500 --> bud; {ENERGY > SIZE * 400 and SIZE < 7} --> grow; ahead[0] < -1 and ENERGY < 500 * SIZE --> eat; (ahead[1] / 10 mod 100) != 17 and ahead[1] > 0 --> attack; ahead[1] < -5 --> forward; ahead[2] < -10 and ahead[1] = 0 --> forward; ahead[3] < -15 and ahead[1] = 0 --> forward; ahead[4] < -20 and ahead[1] = 0 --> forward; nearby[0] > 0 and nearby[3] = 0 --> backward; ahead[1] < -1 and { ENERGY > 2500 or SIZE > 7 } --> serve[ENERGY / 42]; random[3] = 1 --> left; 1 = 1 --> wait;");
@@ -28,13 +28,17 @@ public class MutationTest {
 		MutRemove mr = new MutRemove();
 		mr.initiate(r);
 		
-		nodeToMutate = r.nodeAt(0);
+		/*nodeToMutate = r.nodeAt(0);
 		mr.findparent(nodeToMutate, 0);
-		System.out.println(mr.Mutate(r.nodeAt(0)));
+		System.out.println(mr.Mutate(r.nodeAt(0)));*/
 	
-		nodeToMutate = r.nodeAt(1);
-		mr.findparent(nodeToMutate, 1);
-		System.out.println(mr.Mutate(r.nodeAt(1)));
+		mr.findparent(r, 1);
+		assertTrue(mr.parent == r);
+		mr.Mutate(r.nodeAt(1));
+		for (int place = 0; place < r.size(); place++){
+			mr.findparent(r, place);
+		}
+		System.out.println(r.prettyPrint(new StringBuilder()));
 		
 		
 		/*for (int them = 0; them < r.size(); them ++){
@@ -68,6 +72,12 @@ public class MutationTest {
 		nodeToMutate = r.nodeAt(1);
 		System.out.println(ms.Mutate(r.nodeAt(1)));
 		
+		
+	}
+	
+	@Ignore
+	@Test
+	public void replace(){
 		
 	}
 	
