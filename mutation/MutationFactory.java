d46685d5d42d60e0da9b2dee39ef96336dbfcc0f
HaberR <rafaelhaber@dhcp-gs-1291.eduroam.cornell.edu>
2015-10-15 15:55:47 -0400

    mutationfactory
    Just adding some methods to make it nice and easy to randomly choose a
    mutation for a node when the time comes.

--- mutation/MutationFactory.java
+++ mutation/MutationFactory.java
@@ -4,15 +4,18 @@ package mutation;
  * A factory that produces the public static Mutation objects corresponding to each
  * mutation
  */
+import java.util.Random;
+
 public class MutationFactory {
+	
+	
+	
     public static Mutation getRemove() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException();
+        return new MutRemove();
     }
 
     public static Mutation getSwap() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException();
+        return new MutSwap();
     }
 
     public static Mutation getReplace() {
@@ -26,12 +29,29 @@ public class MutationFactory {
     }
 
     public static Mutation getInsert() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException();
+        return new MutInsert();
     }
 
     public static Mutation getDuplicate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException();
+    	return new MutDuplicate();
+    }
+    
+    /** Returns an array of all the mutation objects*/
+    public static Mutation[] allMuts(){
+    	Mutation mutone = getRemove();
+    	Mutation muttwo = getSwap();
+    	Mutation mutthree = getReplace();
+    	Mutation mutfour = getTransform();
+    	Mutation mutfive = getInsert();
+    	Mutation mutsix = getDuplicate();
+    	return new Mutation [] {mutone, muttwo,
+    			mutthree, mutfour, mutfive, mutsix
+    	};
+    	
+    }
+    
+    /** effect: randomizes an array of mutation objects*/
+    public static void randMutation(Mutation [] total){
+    	Random rand = new Random();
     }
 }
