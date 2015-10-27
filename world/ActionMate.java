07de049851a16ecbe09d8b2cce1664f463f825b4
HaberR <rafaelhaber@dhcp-legacy-1189.eduroam.cornell.edu>
2015-10-27 18:41:51 -0400

    critter added to critter list
    New critters made through mating/budding are now added to the critter
    list given that they come into existence.

--- world/ActionMate.java
+++ world/ActionMate.java
@@ -46,6 +46,7 @@ public class ActionMate {
 					mutate(baby);
 					place(baby,c, specific);
 					baby.name = "childof(" + c.name + ", " + specific.name + ")"; 
+					c.w.addCritter(baby);
 				}
 			}
 		}
@@ -187,6 +188,7 @@ public class ActionMate {
 			if (Crittermethods.checkempty(c, false)) {
 				int [] loc = Crittermethods.dircoords(c, false);
 				c.w.replace(k, c.w.getHex(loc[0], loc[1]));
+				c.w.addCritter(k);
 			}
 		}
 	}
