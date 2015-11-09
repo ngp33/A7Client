065fb751f663225b2a9ef7d276fe635ae766738a
Neil Patel <npatel02468@gmail.com>
2015-11-09 00:12:01 -0500

    Added functionality to step button + file menu
    Waiting for A5 comments before I add the load world and load critter
    code. Only dialogs pop up for now.

--- gui/Controller.java
+++ gui/Controller.java
@@ -1,6 +1,9 @@
 package gui;
 
+import java.io.File;
 import java.util.Date;
+import java.util.Optional;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -8,22 +11,49 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import world.Food;
+import world.Rock;
 import world.World;
 
 public class Controller {
 	
-	Scene view;
+	Stage view;
 	World model;
 	Timer timer;
 	
-	public Controller(Scene v, World m) {
+	public Controller(Stage v, World m) {
 		view = v;
 		model = m;
 		
-		Button playAndPause = (Button) view.lookup("#play");
+		Scene scene = v.getScene();
 		
+		Button playAndPause = (Button) scene.lookup("#play");
 		playAndPause.setOnAction(new playPauseHandler());
+		
+		Button step = (Button) scene.lookup("#step");
+		step.setOnAction(new StepHandler());
+		
+		
+		MenuBar topBar = (MenuBar) scene.lookup("#topbar");
+		Menu file = topBar.getMenus().get(0);
+		Menu help = topBar.getMenus().get(1);
+		
+		MenuItem newWorld = file.getItems().get(0);
+		newWorld.setOnAction(new NewWorldHandler());
+		
+		MenuItem loadWorld = file.getItems().get(1);
+		loadWorld.setOnAction(new LoadWorldHandler());
+		
+		MenuItem loadCritter = file.getItems().get(2);
+		loadCritter.setOnAction(new LoadCritterHandler());
 		
 	}
 
@@ -40,7 +70,7 @@ public class Controller {
 			} else {
 				playing = true;
 				
-				TextField speedInput = (TextField) view.lookup("#speed");
+				TextField speedInput = (TextField) view.getScene().lookup("#speed");
 				String speedStr = speedInput.getText();
 				double freq = Double.parseDouble(speedStr);
 				long period = (long) (1000d/freq + .5d); //Round
@@ -59,6 +89,88 @@ public class Controller {
 			model.advance();
 			
 			//Update view from here or call a method in World?
+		}
+		
+	}
+	
+	class StepHandler implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent arg0) {
+			model.advance();
+			
+			//Update view from here or call a method in World? I think World would be better to avoid repeated code.
+		}
+		
+	}
+	
+	class NewWorldHandler implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+			model = new World();
+	    	
+	    	int rows = model.getNumRows();
+	    	int cols = model.getNumColumns();
+	    	
+	    	Random rand = new Random();
+	    	
+	    	for (int i = 0; i < cols; i++) {
+	    		for (int j = 0; j < rows; j++) {
+	    			model.setHex(j, i, rand.nextFloat() < .15 ? new Rock() : new Food(0)); //15% chance of Rock
+	    		}
+	    	}
+		}
+		
+	}
+	
+	class LoadWorldHandler implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Load world");
+			fileChooser.getExtensionFilters().add(new ExtensionFilter("World Files", "*.txt"));
+			File selectedFile = fileChooser.showOpenDialog(view);
+			
+			if (selectedFile == null) {
+				return;
+			}
+			
+			//Reluctant to copy and paste load code from Console until we get A5 back.
+		}
+		
+	}
+	
+	class LoadCritterHandler implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Load critter");
+			fileChooser.getExtensionFilters().add(new ExtensionFilter("World Files", "*.txt"));
+			File selectedFile = fileChooser.showOpenDialog(view);
+			
+			if (selectedFile == null) {
+				return;
+			}
+			
+			TextInputDialog numCrittersDialog = new TextInputDialog();
+			numCrittersDialog.initOwner(view);
+			numCrittersDialog.setContentText("How many critters?");
+			numCrittersDialog.setHeaderText(null);
+			numCrittersDialog.setGraphic(null);
+			numCrittersDialog.setTitle("Load critter");
+			Optional<String> input = numCrittersDialog.showAndWait();
+			
+			if (!input.isPresent()) {
+				return;
+			}
+			
+			String inputStr = input.get();
+			int numCritters = Integer.parseInt(inputStr);
+			
+			//Reluctant to copy and paste load code from Console until we get A5 back.
 		}
 		
 	}
--- gui/Main.java
+++ gui/Main.java
@@ -13,9 +13,9 @@ public class Main extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		Scene scene = new Scene(root);
 		
-		Controller c = new Controller(scene, new World());
-		
 		primaryStage.setScene(scene);
+		
+		Controller c = new Controller(primaryStage, new World());
 		
 		primaryStage.show();
 	}
